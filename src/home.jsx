940fb160c97b7c639194c1c27beda996e6867d52
brazill91 <brazill91@gmail.com>
2020-08-11 16:09:01 -0400

    pre-styling

---
+++ src/home.jsx
@@ -0,0 +1,337 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import "./home.css";
+
+class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="home">
+        <div className="rectangle1"></div>
+        <div className="group2">
+          <div className="nw9b39fb13">NW</div>
+          <div className="livingconstructiona93352ad">
+            LIVING
+            <br />
+            CONSTRUCTION
+          </div>
+        </div>
+        <div className="group42">
+          {" "}
+          <div className="group1">
+            {" "}
+            <div className="homebba356fe">HOME</div>
+            <div className="aboutbdff10d8">ABOUT</div>
+            <div className="servicesbc8eec15">SERVICES</div>
+            <div className="projects1534629d">PROJECTS</div>
+            <div className="contact">CONTACT</div>
+          </div>
+          <svg preserveAspectRatio="none" viewBox="0 -1 56 2" className="line1">
+            <path d="M 0 0 L 56 0" />
+          </svg>
+        </div>
+        <div className="group4">
+          {" "}
+          <div className="rectangle2"></div>
+          <div className="phone"></div>
+          <div className="x9876543210">9876543210</div>
+        </div>
+        <div className="maskGroup1"></div>
+        <div className="rectangle4"></div>
+        <div className="group7">
+          {" "}
+          <div className="group5">
+            {" "}
+            <div className="weReBuildYour">We Re-build your</div>
+            <div className="dreamHomes">DREAM HOMES</div>
+            <div className="loremIpsumDolorSitAmetConsecteturAdipiscingElitLoremIpsumDolorSitAmetConsecteturAdipiscingElitLoremIpsumDolorSitAmetConsecteturAdipiscingElit">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem
+              ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum
+              dolor sit amet, consectetur adipiscing elit.
+            </div>
+          </div>
+          <div className="group6">
+            {" "}
+            <div className="rectangle5e3cd30b0"></div>
+            <div className="contactUs6725f55b">CONTACT US</div>
+          </div>
+        </div>
+        <div className="group15">
+          {" "}
+          <div className="rectangle6e904d840"></div>
+          <svg
+            preserveAspectRatio="none"
+            viewBox="0 0 422 179"
+            className="path265c0dd9b"
+          >
+            <path d="M 0 0 L 422 0 L 335.1560668945313 179 L 0 179 L 0 0 Z" />
+          </svg>
+          <svg
+            preserveAspectRatio="none"
+            viewBox="0 0 422 179"
+            className="path17777ed57a"
+          >
+            <path d="M 0 0 L 422 0 L 335.1560668945313 179 L 0 179 L 0 0 Z" />
+          </svg>
+          <svg
+            preserveAspectRatio="none"
+            viewBox="0 0 422 179"
+            className="path3"
+          >
+            <path d="M 422 179 L 0 179 L 86.84393310546875 0 L 422 0 L 422 179 Z" />
+          </svg>
+          <div className="group11">
+            {" "}
+            <div className="x16Awards">16 AWARDS</div>
+            <div className="nounaward1539618"></div>
+          </div>
+          <div className="group12">
+            {" "}
+            <div className="x200ProjectsCompleted">200+ PROJECTS COMPLETED</div>
+            <div className="nounhome323088"></div>
+          </div>
+          <div className="group13">
+            {" "}
+            <div className="x70Clients">70+ CLIENTS</div>
+            <div className="nounusers749183"></div>
+          </div>
+        </div>
+        <div className="group23">
+          {" "}
+          <div className="rectangle6"></div>
+          <svg
+            preserveAspectRatio="none"
+            viewBox="0 0 422 231.736328125"
+            className="path2"
+          >
+            <path d="M 0 0 L 422 0 L 335.1560668945313 231.736328125 L 0 231.736328125 L 0 0 Z" />
+          </svg>
+          <svg
+            preserveAspectRatio="none"
+            viewBox="0 0 422 231.736328125"
+            className="path17"
+          >
+            <path d="M 0 0 L 422 0 L 335.1560668945313 231.736328125 L 0 231.736328125 L 0 0 Z" />
+          </svg>
+        </div>
+        <div className="group16">
+          {" "}
+          <div className="outsiteCoRLk3sqlibwUnsplash"></div>
+          <div className="about">ABOUT</div>
+          <div className="loremIpsumDolorSitAmetConsecteturAdipiscingElitSedDoEiusmodTemporIncididuntUtLaboreEtDoloreMagnaAliquaUtEnimAdMinimVeniamQuisNostrudExercitationUllamcoLaborisNisiUtAliquipExEaCommodoConsequatDuisAuteIrureDolorIn97d1f490">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum. Excepteur
+            sint occaecat cupidatat non proident, sunt in culpa.{" "}
+          </div>
+        </div>
+        <div className="group22">
+          {" "}
+          <div className="group17">
+            {" "}
+            <div className="our">OUR</div>
+            <div className="services">SERVICES</div>
+          </div>
+          <div className="group18">
+            {" "}
+            <svg
+              preserveAspectRatio="none"
+              viewBox="-31 0 409.4993896484375 79.715576171875"
+              className="path183381cc1b"
+            >
+              <path d="M -3.542412757873535 0 L 378.4993896484375 0 L 350.7490234375 79.715576171875 L -31 79.715576171875 L -3.542412757873535 0 Z" />
+            </svg>
+            <svg
+              preserveAspectRatio="none"
+              viewBox="-31.75 -0.75 410.9993896484375 81.215576171875"
+              className="path18ee29d597"
+            >
+              <path d="M -3.542412757873535 0 L 378.4993896484375 0 L 350.7490234375 79.715576171875 L -31 79.715576171875 L -3.542412757873535 0 Z" />
+            </svg>
+            <svg
+              preserveAspectRatio="none"
+              viewBox="-31.75 -0.75 410.99945068359375 81.215576171875"
+              className="path18725e6eb5"
+            >
+              <path d="M -3.542412757873535 0 L 378.4993896484375 0 L 350.7490234375 79.715576171875 L -31 79.715576171875 L -3.542412757873535 0 Z" />
+            </svg>
+            <svg
+              preserveAspectRatio="none"
+              viewBox="-31.75 -0.75 410.9993896484375 81.215576171875"
+              className="path18"
+            >
+              <path d="M -3.542412757873535 0 L 378.4993896484375 0 L 350.7490234375 79.715576171875 L -31 79.715576171875 L -3.542412757873535 0 Z" />
+            </svg>
+            <div className="drywall">DRYWALL</div>
+            <div className="taping">TAPING</div>
+            <div className="simpleFraming">SIMPLE FRAMING</div>
+            <div className="renovation">RENOVATION</div>
+          </div>
+          <div className="group21">
+            {" "}
+            <div className="rectangle7"></div>
+            <div className="charlesDeluvioZa9oagrjnlmUnsplash"></div>
+            <div className="group20">
+              {" "}
+              <div className="drywallServices">DRYWALL SERVICES</div>
+              <div className="loremIpsumDolorSitAmetConsecteturAdipiscingElitSedDoEiusmodTemporIncididuntUtLaboreEtDoloreMagnaAliquaUtEnimAdMinimVeniamQuisNostrudExercitationUllamcoLaborisNisiUtAliquipExEaCommodoConsequatDuisAuteIrureDolorIn">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur.
+              </div>
+              <div className="group19">
+                {" "}
+                <div className="rectangle543373888"></div>
+                <div className="contactUsd43b769d">CONTACT US</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="group25">
+          {" "}
+          <div className="getYourFreeConsultationNow">
+            GET YOUR FREE CONSULTATION NOW !
+          </div>
+          <div className="weWillHelpYouReBuildEverythingAsPerYourRequirementsWeAreACompanyThatOffersDesignAndBuildServicesForYouFromInitialSketchesToTheFinalConstruction">
+            We will help you re-build everything as per your requirements. We
+            are a company that offers design and build services for you from
+            initial sketches to the final construction.
+          </div>
+        </div>
+        <div className="group24">
+          {" "}
+          <div className="rectangle8"></div>
+          <div className="contactUs8e14d283">CONTACT US</div>
+        </div>
+        <div className="group26">
+          {" "}
+          <div className="featured">FEATURED</div>
+          <div className="projects">PROJECTS</div>
+        </div>
+        <div className="group29">
+          {" "}
+          <div className="contactUs">CONTACT US</div>
+        </div>
+        <div className="group28">
+          {" "}
+          <div className="group2748095ba0">
+            {" "}
+            <div className="rectangle97536b16f"></div>
+            <div className="jasonBriscoeUv81e0oxxwqUnsplash"></div>
+            <div className="projectTitle1809ccb7">PROJECT TITLE</div>
+            <div className="loremIpsumDolorSitAmetConsecteturAdipiscingElitSedDoEiusmodTemporIncididuntUtLaboreEtDoloreMagnaAliqua35108073">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </div>
+          </div>
+          <div className="group27a903b0f9">
+            {" "}
+            <div className="rectangle9d04ba13a"></div>
+            <div className="danielMcculloughHtblqdxfg9kUnsplash9f2c92ca"></div>
+            <div className="projectTitledb4f9228">PROJECT TITLE</div>
+            <div className="loremIpsumDolorSitAmetConsecteturAdipiscingElitSedDoEiusmodTemporIncididuntUtLaboreEtDoloreMagnaAliqua6f8c9759">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </div>
+          </div>
+          <div className="group27">
+            {" "}
+            <div className="rectangle9"></div>
+            <div className="annieGrayWewtgkpuvt0Unsplash"></div>
+            <div className="projectTitle">PROJECT TITLE</div>
+            <div className="loremIpsumDolorSitAmetConsecteturAdipiscingElitSedDoEiusmodTemporIncididuntUtLaboreEtDoloreMagnaAliqua">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </div>
+          </div>
+        </div>
+        <div className="group33">
+          {" "}
+          <div className="danielMcculloughHtblqdxfg9kUnsplash"></div>
+          <div className="rectangle10"></div>
+          <div className="group32">
+            {" "}
+            <div className="group31">
+              {" "}
+              <div className="rectangle11febd3a83"></div>
+              <div className="rectangle1122c52bb2"></div>
+              <div className="rectangle117a9306d7"></div>
+              <div className="rectangle11261eb42f"></div>
+              <div className="rectangle11"></div>
+              <div className="firstName">FIRST NAME</div>
+              <div className="phoneNumber">PHONE NUMBER</div>
+              <div className="yourRemarksHere">YOUR REMARKS HERE</div>
+              <div className="lastName">LAST NAME</div>
+              <div className="emailAddress">EMAIL ADDRESS</div>
+            </div>
+            <div className="group30">
+              {" "}
+              <div className="rectangle5"></div>
+              <div className="submit">SUBMIT</div>
+            </div>
+          </div>
+        </div>
+        <div className="group41">
+          {" "}
+          <div className="screenshotWwwgooglecom20200725000755"></div>
+          <div className="rectangle12"></div>
+        </div>
+        <div className="group40">
+          {" "}
+          <div className="rectangle13"></div>
+          <div className="group36">
+            {" "}
+            <div className="loremIpsumDolorSitAmetConsecteturAdipisicin">
+              Lorem ipsum dolor sit amet, consectetur adipisicin
+            </div>
+            <div className="address">Address :</div>
+          </div>
+          <div className="followForLatestUpdates">
+            FOLLOW FOR LATEST UPDATES :
+          </div>
+          <div className="group38">
+            {" "}
+            <div className="facebook1"></div>
+            <div className="instagram"></div>
+            <div className="twitter"></div>
+          </div>
+          <div className="emailContact">Email : contact@xstudio.com</div>
+          <div className="phone2468465">Phone : 2468465</div>
+          <div className="group40Eaf1625de6e3"></div>
+          <div className="allRightsReserved2020">
+            All Rights Reserved
+            <br />
+            2020
+          </div>
+          <div className="group39">
+            {" "}
+            <div className="nw">NW</div>
+            <div className="livingconstruction">
+              LIVING
+              <br />
+              CONSTRUCTION
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+Home.propTypes = {};
+
+Home.defaultProps = {};
+
+export default Home;
