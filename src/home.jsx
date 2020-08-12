02b84ecfd0c1e39b75483e7fd58416d4fed27da7
brazill91 <brazill91@gmail.com>
2020-08-11 20:30:56 -0400

    bootstrapping page sections

--- src/home.jsx
+++ src/home.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import PropTypes from "prop-types";
-
+import sawImg from "./assets/maskGroup1.png"
+import aboutImage from "./assets/outsiteCoRLk3sqlibwUnsplash.png"
+import project1 from "./assets/jasonBriscoeUv81e0oxxwqUnsplash.png"
+import project2 from "./assets/danielMcculloughHtblqdxfg9kUnsplash9f2c92ca.png"
+import project3 from "./assets/annieGrayWewtgkpuvt0Unsplash.png"
 import "./home.css";
 
 class Home extends React.Component {
@@ -21,15 +25,15 @@ class Home extends React.Component {
             CONSTRUCTION
           </div>
         </div>
-        <div className="group42">
+        <div id="nav-bar" className="container">
           {" "}
-          <div className="group1">
-            {" "}
-            <div className="homebba356fe">HOME</div>
-            <div className="aboutbdff10d8">ABOUT</div>
-            <div className="servicesbc8eec15">SERVICES</div>
-            <div className="projects1534629d">PROJECTS</div>
-            <div className="contact">CONTACT</div>
+          <div className="row d-flex flex-row">
+            
+            <div className="col nav-item">HOME</div>
+            <div className="col nav-item">ABOUT</div>
+            <div className="col nav-item">SERVICES</div>
+            <div className="col nav-item">PROJECTS</div>
+            <div className="col nav-item">CONTACT</div>
           </div>
           <svg preserveAspectRatio="none" viewBox="0 -1 56 2" className="line1">
             <path d="M 0 0 L 56 0" />
@@ -41,27 +45,37 @@ class Home extends React.Component {
           <div className="phone"></div>
           <div className="x9876543210">9876543210</div>
         </div>
-        <div className="maskGroup1"></div>
-        <div className="rectangle4"></div>
-        <div className="group7">
-          {" "}
-          <div className="group5">
-            {" "}
-            <div className="weReBuildYour">We Re-build your</div>
-            <div className="dreamHomes">DREAM HOMES</div>
-            <div className="loremIpsumDolorSitAmetConsecteturAdipiscingElitLoremIpsumDolorSitAmetConsecteturAdipiscingElitLoremIpsumDolorSitAmetConsecteturAdipiscingElit">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem
-              ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum
-              dolor sit amet, consectetur adipiscing elit.
+
+         {/* Dream Home */}
+        <div className="container-fluid dream-homes-area">
+          <div id="dream-homes-content" className="dream-homes-content container position-absolute">
+            {/* <div className="dream-homes-rectangle"></div> */}
+            <div className="group5">
+              <div className="col weReBuildYour">We Re-build your</div>
+              <div className="col dreamHomes">DREAM HOMES</div>
+              <div className="col sample-text">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem
+                ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum
+                dolor sit amet, consectetur adipiscing elit.
+              </div>
+              {/* contact button */}
+              <div className="col-4 btn-contact">
+                <button className="btn btn-default">CONTACT US</button>
+              </div>
+            </div>
+
+          </div>
+          {/* bg image */}
+          <div className="bg-dream-homes row">
+            <div className="col">
+              <img className="img" src={sawImg} alt=""></img>
             </div>
           </div>
-          <div className="group6">
-            {" "}
-            <div className="rectangle5e3cd30b0"></div>
-            <div className="contactUs6725f55b">CONTACT US</div>
-          </div>
+
+         
+          
         </div>
-        <div className="group15">
+        {/* <div className="group15">
           {" "}
           <div className="rectangle6e904d840"></div>
           <svg
@@ -100,7 +114,7 @@ class Home extends React.Component {
             <div className="x70Clients">70+ CLIENTS</div>
             <div className="nounusers749183"></div>
           </div>
-        </div>
+        </div> */}
         <div className="group23">
           {" "}
           <div className="rectangle6"></div>
@@ -119,31 +133,43 @@ class Home extends React.Component {
             <path d="M 0 0 L 422 0 L 335.1560668945313 231.736328125 L 0 231.736328125 L 0 0 Z" />
           </svg>
         </div>
-        <div className="group16">
-          {" "}
-          <div className="outsiteCoRLk3sqlibwUnsplash"></div>
-          <div className="about">ABOUT</div>
-          <div className="loremIpsumDolorSitAmetConsecteturAdipiscingElitSedDoEiusmodTemporIncididuntUtLaboreEtDoloreMagnaAliquaUtEnimAdMinimVeniamQuisNostrudExercitationUllamcoLaborisNisiUtAliquipExEaCommodoConsequatDuisAuteIrureDolorIn97d1f490">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum. Excepteur
-            sint occaecat cupidatat non proident, sunt in culpa.{" "}
+
+        {/* About Area */}
+        <div className="container-fluid">
+          <div className='row'>
+            {/* bg image */}
+            <div className="col-6 outsiteCoRLk3sqlibwUnsplash">
+              <img className="img" src={aboutImage} alt=""></img>
+            </div>
+            {/* text */}
+            <div className='col-6'>
+              <div className='container'>
+                <div className='row d-flex flex-column'>
+                  <div className="col about">ABOUT</div>
+                  <div className="col loremIpsumDolorSitAmetConsecteturAdipiscingElitSedDoEiusmodTemporIncididuntUtLaboreEtDoloreMagnaAliquaUtEnimAdMinimVeniamQuisNostrudExercitationUllamcoLaborisNisiUtAliquipExEaCommodoConsequatDuisAuteIrureDolorIn97d1f490">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+                    ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                    aliquip ex ea commodo consequat. Duis aute irure dolor in
+                    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                    pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                    culpa qui officia deserunt mollit anim id est laborum. Excepteur
+                    sint occaecat cupidatat non proident, sunt in culpa.{" "}
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
-        <div className="group22">
-          {" "}
-          <div className="group17">
-            {" "}
-            <div className="our">OUR</div>
-            <div className="services">SERVICES</div>
+
+        {/* Our Services */}
+        <div className="container-fluid group22">
+          <div className="row group17">
+            <div className="col">OUR</div>
+            <div className="col">SERVICES</div>
           </div>
-          <div className="group18">
-            {" "}
-            <svg
+          <div className="row group18">
+            {/* <svg
               preserveAspectRatio="none"
               viewBox="-31 0 409.4993896484375 79.715576171875"
               className="path183381cc1b"
@@ -170,11 +196,12 @@ class Home extends React.Component {
               className="path18"
             >
               <path d="M -3.542412757873535 0 L 378.4993896484375 0 L 350.7490234375 79.715576171875 L -31 79.715576171875 L -3.542412757873535 0 Z" />
-            </svg>
-            <div className="drywall">DRYWALL</div>
-            <div className="taping">TAPING</div>
-            <div className="simpleFraming">SIMPLE FRAMING</div>
-            <div className="renovation">RENOVATION</div>
+            </svg> */}
+            
+            <div className="col services-bar">DRYWALL</div>
+            <div className="col services-bar">TAPING</div>
+            <div className="col services-bar">SIMPLE FRAMING</div>
+            <div className="col services-bar">RENOVATION</div>
           </div>
           <div className="group21">
             {" "}
@@ -199,6 +226,8 @@ class Home extends React.Component {
             </div>
           </div>
         </div>
+
+
         <div className="group25">
           {" "}
           <div className="getYourFreeConsultationNow">
@@ -210,79 +239,90 @@ class Home extends React.Component {
             initial sketches to the final construction.
           </div>
         </div>
+
+        {/* Contact Us */}
         <div className="group24">
-          {" "}
           <div className="rectangle8"></div>
           <div className="contactUs8e14d283">CONTACT US</div>
         </div>
-        <div className="group26">
-          {" "}
-          <div className="featured">FEATURED</div>
-          <div className="projects">PROJECTS</div>
-        </div>
-        <div className="group29">
-          {" "}
-          <div className="contactUs">CONTACT US</div>
-        </div>
-        <div className="group28">
-          {" "}
-          <div className="group2748095ba0">
-            {" "}
-            <div className="rectangle97536b16f"></div>
-            <div className="jasonBriscoeUv81e0oxxwqUnsplash"></div>
-            <div className="projectTitle1809ccb7">PROJECT TITLE</div>
-            <div className="loremIpsumDolorSitAmetConsecteturAdipiscingElitSedDoEiusmodTemporIncididuntUtLaboreEtDoloreMagnaAliqua35108073">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            </div>
+
+        {/* Featured Projects */}
+        <div className="container">
+          <div className='row'>
+            <div className="col">FEATURED</div>
+            <div className="col">PROJECTS</div>
           </div>
-          <div className="group27a903b0f9">
-            {" "}
-            <div className="rectangle9d04ba13a"></div>
-            <div className="danielMcculloughHtblqdxfg9kUnsplash9f2c92ca"></div>
-            <div className="projectTitledb4f9228">PROJECT TITLE</div>
-            <div className="loremIpsumDolorSitAmetConsecteturAdipiscingElitSedDoEiusmodTemporIncididuntUtLaboreEtDoloreMagnaAliqua6f8c9759">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          {/* samples */}
+          <div className="row">
+            <div className='col-4'>
+              <img className='img' src={project1} alt=''></img>
+            
+              {/* project1 */}
+              <div className="projectTitle1809ccb7">PROJECT TITLE</div>
+              <div className="loremIpsumDolorSitAmetConsecteturAdipiscingElitSedDoEiusmodTemporIncididuntUtLaboreEtDoloreMagnaAliqua35108073">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </div>
             </div>
-          </div>
-          <div className="group27">
-            {" "}
-            <div className="rectangle9"></div>
-            <div className="annieGrayWewtgkpuvt0Unsplash"></div>
-            <div className="projectTitle">PROJECT TITLE</div>
-            <div className="loremIpsumDolorSitAmetConsecteturAdipiscingElitSedDoEiusmodTemporIncididuntUtLaboreEtDoloreMagnaAliqua">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+
+              {/* project2 */}
+            <div className='col-4'>
+              <img className='img' src={project2} alt=''></img>
+              <div className="projectTitle1809ccb7">PROJECT TITLE</div>
+              <div className="loremIpsumDolorSitAmetConsecteturAdipiscingElitSedDoEiusmodTemporIncididuntUtLaboreEtDoloreMagnaAliqua35108073">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </div>
             </div>
-          </div>
-        </div>
-        <div className="group33">
-          {" "}
-          <div className="danielMcculloughHtblqdxfg9kUnsplash"></div>
-          <div className="rectangle10"></div>
-          <div className="group32">
-            {" "}
-            <div className="group31">
-              {" "}
-              <div className="rectangle11febd3a83"></div>
-              <div className="rectangle1122c52bb2"></div>
-              <div className="rectangle117a9306d7"></div>
-              <div className="rectangle11261eb42f"></div>
-              <div className="rectangle11"></div>
-              <div className="firstName">FIRST NAME</div>
-              <div className="phoneNumber">PHONE NUMBER</div>
-              <div className="yourRemarksHere">YOUR REMARKS HERE</div>
-              <div className="lastName">LAST NAME</div>
-              <div className="emailAddress">EMAIL ADDRESS</div>
-            </div>
-            <div className="group30">
-              {" "}
-              <div className="rectangle5"></div>
-              <div className="submit">SUBMIT</div>
+            
+              {/* project3 */}
+            <div className='col-4'>
+              <img className='img' src={project3} alt=''></img>
+              <div className="projectTitle1809ccb7">PROJECT TITLE</div>
+              <div className="loremIpsumDolorSitAmetConsecteturAdipiscingElitSedDoEiusmodTemporIncididuntUtLaboreEtDoloreMagnaAliqua35108073">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </div>
             </div>
           </div>
         </div>
+
+        
+
+        {/* Contact Us */}
+        <div className="container">
+          <div className="row">
+            <div className='col'>CONTACT US</div>
+          </div>
+        </div>
+        
+        {/* contact form */}
+        <div className="container">
+
+          {/* bg image */}
+          <div className="row">
+            <div className='col'>
+              <img className='img' src={project2} alt=''></img>
+            </div>
+          </div>
+
+          {/* form */}
+          <div className="row">
+            <div className="col-6">FIRST NAME</div>
+            <div className="col-6">PHONE NUMBER</div>
+            <div className="col-6">LAST NAME</div>
+            <div className="col-6">EMAIL ADDRESS</div>
+          </div>
+
+          {/* submit */}
+          <div className="row">
+            <div className="col submit">
+              <button class='btn-main'>SUBMIT</button>
+            </div>
+          </div>
+        </div>
+
+
         <div className="group41">
           {" "}
           <div className="screenshotWwwgooglecom20200725000755"></div>
